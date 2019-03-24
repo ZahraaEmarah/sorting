610b1da9d6fbd82bdf58de0d6306e8c3c85e24e2
Mayar's Laptop <Mayar's Laptop@MAYAR-PC>
2019-03-24 20:12:59 +0200

    Selection Sort

--- Sorting Techniques/src/main.java
+++ Sorting Techniques/src/main.java
@@ -2,16 +2,20 @@
 public class main {
 
 	public static void main(String[] args) {
-		int arr[]= {9,6,5,0,8,2,7,1,3};
+		int arr[]= {9,4,6,10,99,0,100,44,78,1,8};
 		Heap heap= new Heap();
 		heap.setHeap(arr);
 		heap.Buildheap(arr);
-	    heap.printHeap();
+	  //  heap.printHeap();
 		////////////////
 		InsertionSort insertionSort = new InsertionSort();
 		insertionSort.setSorted(arr);
 		insertionSort.inSort();
-		insertionSort.printInsertionSort();
+	//	insertionSort.printInsertionSort();
+		SelectionSort selectionSort = new SelectionSort();
+		selectionSort.setSorted(arr);
+		selectionSort.selection();
+		selectionSort.print();
 		}
 
 }
